084cfea8e6646a6f1d7b5e8cd56b2b1e38fe6004
srnovak13 <srnovak@gmail.com>
2021-07-20 13:20:09 -0600

    Add Car Insurance Program

---
+++ Car_Insurance_Program/Car_Insurance_Program/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Insurance_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get info
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? True or False");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt16(Console.ReadLine());
+
+            //show calculations to see if qualified for insurance
+            bool qualified = (age > 15) && (!DUI) && (tickets <= 3);
+
+            //display result
+            Console.WriteLine("Are you qualified? \n" + qualified);
+            Console.ReadLine();
+
+        }
+    }
+}
