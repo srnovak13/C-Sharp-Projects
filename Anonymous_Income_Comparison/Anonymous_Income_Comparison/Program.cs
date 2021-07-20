6eda215cc9aadc09aaea03e58d6903b80e20ee50
srnovak13 <srnovak@gmail.com>
2021-07-20 12:29:05 -0600

    Add Anonymous Income Comparison assignment

---
+++ Anonymous_Income_Comparison/Anonymous_Income_Comparison/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_Income_Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //introduction statement
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            //obtain information
+
+            //person 1
+            Console.WriteLine("Person 1 " + "\nWhat is your hourly rate?");
+            string person1Rate = Console.ReadLine();
+            double person1HourlyRate = Convert.ToDouble(person1Rate);//convert to double in case of any cents made
+
+            Console.WriteLine("What is your hours worked per week?");
+            string person1Hours = Console.ReadLine();
+            double person1HoursWorked = Convert.ToDouble(person1Hours);
+
+            //person 2
+            Console.WriteLine("Person 2 " + "\nWhat is your hourly rate?");
+            string person2Rate = Console.ReadLine();
+            double person2HourlyRate = Convert.ToDouble(person2Rate);//convert to double in case of any cents made
+
+            Console.WriteLine("What is your hours worked per week?");
+            string person2Hours = Console.ReadLine();
+            double person2HoursWorked = Convert.ToDouble(person2Hours);
+
+            //do math operations for display
+            double person1Salary = person1HourlyRate * person1HoursWorked;
+            double person2Salary = person2HourlyRate * person2HoursWorked;
+            bool salaryComparison = person1Salary > person2Salary;
+
+            //write results to console
+            Console.WriteLine
+                ("Annual Salary of Person 1: " + person1Salary +
+                 "\nAnnual Salary of Person 2: " + person2Salary +
+                 "\nDoes Person 1 make more than Person 2? " + salaryComparison);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Anonymous_Income_Comparison/Anonymous_Income_Comparison/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Anonymous_Income_Comparison")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Anonymous_Income_Comparison")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b6396a7d-e674-407a-a237-4b75119d0af4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
