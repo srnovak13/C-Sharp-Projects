cf7c0ede89e6aa70e1ca45d2761c6b4cff6946db
srnovak13 <srnovak@gmail.com>
2021-07-22 11:32:41 -0600

    Add String Functions Assignment

---
+++ StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Steve";
+            string lastName = "Novak";
+            string currentCity = "Jackson, WY";
+            string status = firstName + " " + lastName + " lives in " + currentCity.ToUpper();
+            StringBuilder newStatus = new StringBuilder();
+            newStatus.Append("I just moved from Oregon!");
+            newStatus.Append("I enjoy living in the Teton Mountains.");
+            newStatus.Append("I go mountain biking every day.");
+
+            Console.WriteLine(newStatus);
+            Console.ReadLine();
+        }
+    }
+}
