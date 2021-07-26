87a64247a7a6d0c06ac5d90e706c45e4eaae627c
srnovak13 <srnovak@gmail.com>
2021-07-26 17:44:00 -0600

    Add OptionalMethod Assignment

---
+++ OptionalMethodAssignment/OptionalMethodAssignment/DefaultMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalMethodAssignment
+{
+    public class DefaultMath
+    {
+        public int Power(int num1, int num2 = 1)
+        {
+            if (num2 == 0)
+            {
+                return 1;
+            }
+            int result = num1;
+            for (int i = 1; i < num2; i++)
+            {
+                result = num1;
+            }
+            return result;
+        }
+    }
+}
---
+++ OptionalMethodAssignment/OptionalMethodAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DefaultMath powerOf = new DefaultMath();
+            Console.WriteLine("Please enter two integers. The first one will be taken to the power of the second." +
+                              "\n If you only enter one integer, it will be taken to the power of one.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int result;
+            try
+            {
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                result = powerOf.Power(num1, num2);
+            }
+
+            catch (Exception)
+            {
+                result = powerOf.Power(num1);
+            }
+            Console.WriteLine("The result is: " + result);
+            Console.ReadLine();
+
+        }
+    }
+}
