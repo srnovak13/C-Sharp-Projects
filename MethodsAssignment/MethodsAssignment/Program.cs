311b18e0ead069eba61d311acdfb89302bb54084
srnovak13 <srnovak@gmail.com>
2021-07-26 12:59:56 -0600

    Add MethodsAssignment

---
+++ MethodsAssignment/MethodsAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get input from user
+            Console.WriteLine("Please enter a digit that you would like to do math with");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            //use methods created in CommonMath class
+            CommonMath doMath = new CommonMath();
+            int square = doMath.Squared(input);
+            int doubled = doMath.Doubled(input);
+            double half = doMath.Halved(input);
+
+            //print to console
+            Console.WriteLine(
+                "Squared: " + square +
+                "Doubled: " + doubled +
+                "Halved: " + half
+                );
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
