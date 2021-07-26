d5c7ca5ebeeec6be3878ccbb627fb8902c259682
srnovak13 <srnovak@gmail.com>
2021-07-26 16:47:42 -0600

    Add Main_Method Assignment

---
+++ Main_Method_Assignment/Main_Method_Assignment/MathEquations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    public class MathEquations
+    {
+        //first method takes int and returns int to power of 3
+        public int PowerThree(int num)
+        {
+            return num * num * num;
+        }
+
+        //second method takes double, returns double to power of 3, divided by 2
+        public double PowerThree(double num)
+        {
+            return (num * num * num) / 2;
+        }
+
+        //third method takes string, converts to int, takes to power of 3 and adds 2
+        public int PowerThree(string numString)
+        {
+            int num = Convert.ToInt32(numString);
+            return (num * num * num) + 2; 
+        }
+    }
+}
---
+++ Main_Method_Assignment/Main_Method_Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class
+            MathEquations doMath = new MathEquations();
+
+            //using three methods from MathEquations class
+            int powerOfThree = doMath.PowerThree(3);
+            double decimalMath = doMath.PowerThree(3.14);
+            int stringConvert = doMath.PowerThree("5");
+
+            //print out to console
+            Console.WriteLine("Three to the power of three: " + powerOfThree);
+            Console.WriteLine("3.14 to the power of three divided by 2: " + decimalMath);
+            Console.WriteLine("\"5\" converted to a string and given power of three plus two: " + stringConvert);
+            Console.ReadLine();
+        }
+    }
+}
