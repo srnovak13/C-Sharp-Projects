ee1fe65a9662525177aa0c9fc3c2fd66eb37b7a6
srnovak13 <srnovak@gmail.com>
2021-07-27 19:48:12 -0600

    Add AbstractClass Assignment

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Test = new Employee() { FirstName = "Sample", LastName = "Student" };
+            Test.SayName();
+            Console.ReadLine();
+        }
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,29 +10,26 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string> { "Jesse", "Bill", "Bob" };
+            game.ListPlayers();
+            Console.ReadLine(); 
+
+            //Deck deck = new Deck();
+            //deck.Shuffle(3);
 
 
-            Deck deck = new Deck();
-            deck.Shuffle(3);
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
 
-
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
-
-            Console.ReadLine();
+            //Console.ReadLine();
         }
 
 
-        //public static Deck Shuffle(Deck deck, int times)
-        //{
-        //    for (int i = 0; i < times; i++)
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
+       
     }
 
     
