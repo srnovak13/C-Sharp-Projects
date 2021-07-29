cc6f369c61ceaab1c66cbe917c8dc8f436439cd1
srnovak13 <srnovak@gmail.com>
2021-07-29 13:08:31 -0600

    Add to EnumParsing

--- EnumParsing/EnumParsing/Program.cs
+++ EnumParsing/EnumParsing/Program.cs
@@ -8,8 +8,36 @@ namespace EnumParsing
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            //get input from user
+            Console.WriteLine("Please enter the current day of the week: ");
+            string TodaysDate = Console.ReadLine();
+
+            //using parse() method
+            try
+            {
+                DaysOfWeek Today = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), TodaysDate);//parses enum and turns input into an enum variable
+                Console.WriteLine(Today);
+                Console.WriteLine((int)Today);
+                Console.ReadLine();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Main(); //recursive call to give user another chance
+            }
+        }
+        public enum DaysOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
         }
     }
+    
 }
