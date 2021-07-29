5c5ec33edb868332ccad4af6438d8cf2fa0810da
srnovak13 <srnovak@gmail.com>
2021-07-29 17:16:00 -0600

    Add Struct Assignment

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number() { Amount = 104.235m };
+            Console.WriteLine($"The number assigned to our struct number is: {num.Amount}");
+            Console.ReadLine();
+        }
+    }
+}
