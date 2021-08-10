f6d73c44c38ae19df41d17c0861c733ffff25030
srnovak13 <srnovak@gmail.com>
2021-08-10 12:34:16 -0600

    Add AllChanges

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //write current date-time to console
+            Console.WriteLine("The current date and time is {0}.", DateTime.Now);
+
+            //get hours to add to current date-time
+            Console.WriteLine("Please enter the number of hours to add to the current datetime: ");
+            double hours = Convert.ToDouble(Console.ReadLine());
+
+            //add hours to the current datetime using AddHours method
+            DateTime projectedTime = DateTime.Now.AddHours(hours);
+
+            //write to console
+            Console.WriteLine($"In {hours} hours, the datetime will be {projectedTime}.");
+            Console.ReadLine();
+        }
+    }
+}
