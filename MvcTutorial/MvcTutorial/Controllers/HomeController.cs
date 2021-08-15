9da5fb796107ef0ed8d083ac5771874c50854210
srnovak13 <srnovak@gmail.com>
2021-08-14 20:33:22 -0600

    Add NewsletterAppMVC

---
+++ MvcTutorial/MvcTutorial/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using MvcTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "Steve";
+            user.LastName = "Novak";
+            user.Age = 40;
+            return View(user);
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Contact.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Contact : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            Label1.Text = TextBox1.Text;
+        }
+    }
+}
