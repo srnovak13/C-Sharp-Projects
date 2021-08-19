e0d8d8f7cfb6fe15e4c59b4655072333fc62da29
srnovak13 <srnovak@gmail.com>
2021-08-19 11:55:55 -0600

    Add CodeFirstApp

---
+++ CodeFirstApp/CodeFirstApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SchoolContext())
+            {
+                //create and save new student
+                Console.WriteLine("Student's First Name: ");
+                var firstName = Console.ReadLine();
+                Console.WriteLine("Student's Last Name: ");
+                var lastName = Console.ReadLine();
+
+                var student = new Student { FirstName = firstName, LastName = lastName };
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+        }
+
+    }
+
+    
+}
---
+++ Scores/Scores/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter in your first name.");
+            string date = DateTime.Today.ToShortDateString();
+            string uName = Console.ReadLine();
+            string msg = $"\nWelcome back, {uName}. Today is {date}";
+            Console.WriteLine(msg);
+
+            string path = @"C:\Users\Steve\GitRepositories\C-Sharp-Projects\Scores\Scores\StudentScores.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+
+            double tScore = 0.0;
+
+            Console.WriteLine("\nStudent scores: \n");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\n" + line);
+                double score = Convert.ToDouble(line);
+                tScore += score;
+            }
+
+            double avgScore = tScore / lines.Length;
+            Console.WriteLine("\nTotal of " + lines.Length + " student scores. \tAverage Score: " + avgScore );
+
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
